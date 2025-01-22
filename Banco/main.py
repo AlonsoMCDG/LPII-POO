9fd975810125c3378b2050198812483a6fcc6ade
Vini <110140005+AlonsoMCDG@users.noreply.github.com>
2025-01-22 09:26:04 -0500

    Remoção do comentário sobre a questão 3 do exercício

--- Banco/main.py
+++ Banco/main.py
@@ -10,21 +10,6 @@
 # funcionalidade de pagar parcelas, diminuindo o 
 # saldo da conta e reduzindo a dívida.
 
-# 3) Crie um sistema para gerencias projetos e 
-# funcionários. O sistema deve permitir que:
-#       - Um funcionário participe de vários projetos.
-#       - Um projeto possa ter vários funcionários.
-#    Os funcionários têm as seguintes 
-# características: ID único, nome e cargo. Cada 
-# projeto tem: id do projeto, nome e descrição. 
-# As informações sobre a associação entre um 
-# funcionário e um projeto incluem: o papel do 
-# funcionário no projeto (ex.: "Desenvolvedor", 
-# "Gerente", "Designer") e a data de início no projeto.
-#    Implemente as seguiintes funcionalidades:
-#       - Crie métodos para adicionar funcionários e projetos ao sistema.
-#       - 
-
 from agencia import Agencia
 
 if __name__ == '__main__':
